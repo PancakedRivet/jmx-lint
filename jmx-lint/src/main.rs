09cc72364f95b206c310c83a208825042abeab1b
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2026-09-02 23:37:24 +1200

    Adding cargo lints

--- jmx-lint/src/main.rs
+++ jmx-lint/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    // greet().unwrap();
 }
+
+// fn greet() -> Result<(), String> {
+//     println!("Hello from the greet function!");
+//     if false {
+//         return Err("An error occurred in greet".into());
+//     }
+//     Ok(())
+// }
